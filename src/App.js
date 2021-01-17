9eec21959fd51d0911726cba51eb53cddaceb99e
nader <nader90akhgari@gmail.com>
2021-01-17 00:11:44 +0000

    add elements for login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const changeHandler = (event) => {
+
+  }
+
+  const submitHandler = (event) => {
+
+  }
   return (
     <div className="App">
-
+      <h2>Sign In</h2>
+      <hr />
+      <form >
+        <label htmlFor='username'>User Name:</label>
+        <input type='text' name='username' placeholder='usertest' onChange={changeHandler}></input>
+        <br />
+        <label htmlFor='password'>Password:</label>
+        <input type='password' name='password' placeholder='Password...' onChange={changeHandler}></input>
+        <br />
+        <button onClick={submitHandler}>Sign In</button>
+      </form>
     </div>
   );
 }
